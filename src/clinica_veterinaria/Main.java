63148c0db972a632e45b2d4e027aeb77497a619c
kerveros12v <kerveros12v@aol.com>
2026-03-22 19:02:29 -0500

    clase main corregido

--- src/clinica_veterinaria/Main.java
+++ src/clinica_veterinaria/Main.java
@@ -14,10 +14,7 @@ public class Main {
         System.out.println("Peso normal : "
                 + mascota.estaEnPesoNormal(20.0, 30.0));
 // ── Integrante 2: Dueno ──────────────────────────────────
-        Dueno dueno = new Dueno("Carlos", "Perez",
-                "0991234567",
-                "carlos@gmail.com",
-                "1756234890");
+        Dueno dueno = new Dueno("Carlos", "Perez", "0991234567", "carlos@gmail.com", "1756234890");
         System.out.println("\n" + dueno.toString());
         System.out.println("Nombre completo : " + dueno.getNombreCompleto());
         System.out.println("Email valido : " + dueno.tieneEmailValido());
@@ -32,8 +29,7 @@ public class Main {
         System.out.println("Es critica : " + consulta.esCritica());
 // ── Integrante 4 (opcional): Veterinario ─────────────────
 // Si el grupo NO tiene Integrante 4, comenta las 4 lineas de abajo
-        Veterinario vet = new Veterinario("Ana", "Gomez",
-                "VET-001", "Cirugia", 7);
+        Veterinaria vet = new Veterinaria("Ana", "Gomez", "VET-001", 7);
         System.out.println("\n" + vet.toString());
         System.out.println("Es especialista : " + vet.esEspecialista());
         System.out.println("\n=====================================");
